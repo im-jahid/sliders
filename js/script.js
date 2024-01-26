7b45875a149fec7f0d10295ce04b136567ad0b39
mdjahidhasandev <jahidhasandev492@gmail.com>
2024-01-26 17:05:56 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+     
+$(document).ready(function(){
+  $('.container').slick({
+  slidesToShow: 5,
+  // autoplay: true,
+  centerMode: true,
+  dots:true,
+  dotscenterMode: true,
+  prevArrow: '.pro-left',
+  nextArrow: '.pro-right',
+  centerPadding: "0px"
+});
+
+});
+$(document).ready(function(){
+  $('.containers').slick({
+  slidesToShow: 5,
+  // autoplay: true,
+  centerMode: true,
+  dots:true,
+  dotscenterMode: true,
+  prevArrow: '.ser-left',
+  nextArrow: '.ser-right',
+  centerPadding: "0px"
+});
+
+});
+    
+$(document).ready(function(){
+  $('.background-wrapper').slick({
+  slidesToShow: 1,
+  autoplay: true,
+  centerMode: true,
+  dots:false,
+  dotscenterMode: true,
+  prevArrow: '.back-left',
+  nextArrow: '.back-right',
+  centerPadding: "0px"
+
+  });
+});
+    
+
